98dc7eb5d70e2d129a1c0cbac435b8b420f98c51
Rick Christenhusz <banana@Fredericks-MBP.fios-router.home>
2018-08-22 17:43:33 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,17 +3,19 @@ console.log("Up and running!");
 // console.log("User flipped " + cardThree);
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = []
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2 ) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	  } else {
-	  	alert("Sorry, try again.");
-	  }
-	
-}
+var checkForMatch = function () {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		  } else {
+		  	alert("Sorry, try again.");
+		  }
+};
+var flipcard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2 ) {
+checkForMatch();		
+	}
+};
+flipcard(0);
+flipcard(2);
