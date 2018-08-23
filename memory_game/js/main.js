f6508751a334fc223bbabbbe026aa8a7d6739ab8
Rick Christenhusz <banana@Fredericks-MBP.fios-router.home>
2018-08-23 17:29:19 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -18,10 +18,10 @@ var cards = [ //cards object
 {
 	rank: "king",
 	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
+	cardImage: "images/king-of-diamonds.png"
 }
 ];
-var cardsInPlay = [] //cards that have been flipped
+var cardsInPlay = []; //cards that have been flipped
 var checkForMatch = function () {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
@@ -29,14 +29,23 @@ var checkForMatch = function () {
 		  	alert("Sorry, try again.");
 		  }
 };
-var flipcard = function (cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+var flipcard = function () {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2 ) {
-checkForMatch();		
+checkForMatch();	// I'd like to find a way to display the match message AFTER the second card is flipped.	
 	}
 };
-flipcard(0); //test to see if it works via the console
-flipcard(2);
+
+var createBoard = function() { //sets up screen for new game
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		var addcard = document.getElementById('game-board');
+		addcard.appendChild(cardElement);
+		cardElement.addEventListener('click', flipcard);
+};
+};
+createBoard();
